0bc2916473e500ce6c3f3ed62d99f631667061d3
Julia Lanu <julialanu@hotmail.com>
2021-11-15 19:21:48 +0100

    Routing a post in progress

--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -4,16 +4,27 @@
 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
 
 <body>
-<div class="container mx-auto my-5">
-    <h1>Posts</h1>
-    <div class="row">
-        <div class="col-6 col-md-4"> Here comes an image</div>
-        <div class="col-md-8">
-            <h2>Title</h2>
-            <p>
-                Aenean varius enim ipsum, quis lobortis nisi consequat a. Pellentesque nibh lorem, posuere et fringilla eget, euismod vitae ligula. Sed porta leo eu nisl bibendum, congue gravida mauris cursus. Sed ut nisi et augue condimentum ultricies. Phasellus feugiat mi id tristique hendrerit. Nulla pharetra varius dignissim. Curabitur pharetra erat et lectus lobortis maximus. Donec id dolor aliquam, semper orci in, congue augue. Vestibulum tempor purus et felis varius.
-            </p>
+    <div class="container mx-auto my-5">
+        <h1>Aquascapes Posts</h1>
+        <div class="row mt-3">
+            <div class="col-6 col-md-4"> Here comes an image</div>
+            <div class="col-md-8">
+                <h2>First post</h2>
+                <p>
+                    Varius enim ipsum, quis lobortis nisi consequat a. Pellentesque nibh lorem, posuere et fringilla eget, euismod vitae ligula. Sed porta leo eu nisl bibendum, congue gravida mauris cursus. Sed ut nisi et augue condimentum ultricies. Phasellus feugiat mi id tristique hendrerit. Nulla pharetra varius dignissim. Curabitur pharetra erat et lectus lobortis maximus. Donec id dolor aliquam, semper orci in, congue augue. Vestibulum tempor purus et felis varius.
+                </p>
+            </div>
+        </div>
+
+        <div class="row mt-3">
+            <div class="col-6 col-md-4"> Here comes an image</div>
+            <div class="col-md-8">
+                <h2>Second post</h2>
+                <p>
+                    Varius enim ipsum, quis lobortis nisi consequat a. Pellentesque nibh lorem, posuere et fringilla eget, euismod vitae ligula. Sed porta leo eu nisl bibendum, congue gravida mauris cursus. Sed ut nisi et augue condimentum ultricies. Phasellus feugiat mi id tristique hendrerit. Nulla pharetra varius dignissim. Curabitur pharetra erat et lectus lobortis maximus. Donec id dolor aliquam, semper orci in, congue augue. Vestibulum tempor purus et felis varius.
+                </p>
+            </div>
         </div>
     </div>
-</div>
+
 </body>
--- routes/web.php
+++ routes/web.php
@@ -20,3 +20,7 @@ Route::get('/', function () {
 Route::get('/welcome', function () {
     return view('welcome');
 });
+
+Route::get('/post', function () {
+    return view('post');
+});
